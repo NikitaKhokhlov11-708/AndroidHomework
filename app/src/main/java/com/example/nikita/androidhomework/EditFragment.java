1c37b6173e1a01a10e5894ccb4e093b9575816b7
Nikita Khokhlov <nkhokhlov11708@gmail.com>
2018-09-30 23:55:02 +0300

    First half

---
+++ app/src/main/java/com/example/nikita/androidhomework/EditFragment.java
@@ -0,0 +1,29 @@
+package com.example.nikita.androidhomework;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditFragment extends DialogFragment implements View.OnClickListener {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getDialog().setTitle("Title!");
+        View v = inflater.inflate(R.layout.edit_fragment, null);
+        return v;
+    }
+
+    public void onClick(View v) {
+        dismiss();
+    }
+
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+    }
+
+    public void onCancel(DialogInterface dialog) {
+        super.onCancel(dialog);
+    }
+}
